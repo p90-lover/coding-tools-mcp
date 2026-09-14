87738c66ec6a68f571fc731823159ae4b12bb133
p90-lover <146848721+p90-lover@users.noreply.github.com>
2026-09-14 18:08:33 +0800

    fix: import Node built-in export synchronization from node:module

--- runtime-web/scripts/no-delete-preload.mjs
+++ runtime-web/scripts/no-delete-preload.mjs
@@ -1,5 +1,6 @@
 import crypto from 'node:crypto';
-import fs, { syncBuiltinESMExports } from 'node:fs';
+import fs from 'node:fs';
+import { syncBuiltinESMExports } from 'node:module';
 import path from 'node:path';
 
 const original = Object.freeze({
