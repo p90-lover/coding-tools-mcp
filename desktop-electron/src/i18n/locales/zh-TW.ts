d6a87ede5c0f29eb66180cc1aa721f97b74d934c
p90-lover <146848721+p90-lover@users.noreply.github.com>
2026-09-16 17:23:38 +0800

    feat(rc2): add traditional chinese locale foundation

---
+++ desktop-electron/src/i18n/locales/zh-TW.ts
@@ -0,0 +1,12 @@
+export default {
+  providers: "提供者",
+  paseoOrchestrator: "Paseo 協調器",
+  annealTasks: "Anneal 任務",
+  networkProxy: "網路代理",
+  usePaseo: "使用 Paseo 作為協調器",
+  dispatchThroughPaseo: "透過 Paseo 執行",
+  proxyEnabled: "啟用代理",
+  github: "GitHub",
+  x: "X",
+  continue: "繼續",
+};
