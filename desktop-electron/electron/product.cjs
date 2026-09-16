4d386c7fdf4db108ec1aed623122a1ddd3098d77
p90-lover <146848721+p90-lover@users.noreply.github.com>
2026-09-16 12:07:09 +0000

    build: align Coding Tools 0.7 package identity

--- desktop-electron/electron/product.cjs
+++ desktop-electron/electron/product.cjs
@@ -3,7 +3,7 @@
 exports.PRODUCT_IDENTITY = Object.freeze({
   appId: "dev.codingtools.fullharness",
   productName: "Coding Tools",
-  version: "0.7.0-rc.1",
+  version: "0.7.0-rc.2",
   protocolVersion: 1,
   connectorName: "Coding Tools Native2",
   devConnectorName: "Coding Tools Native2 DEV",
