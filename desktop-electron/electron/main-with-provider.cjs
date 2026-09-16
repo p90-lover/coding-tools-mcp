5e8c4f6bb58f3b7810a469cbd987a31ec0d16b32
p90-lover <146848721+p90-lover@users.noreply.github.com>
2026-09-16 21:41:46 +0800

    feat(provider-hub): bind planner to live headless host

--- desktop-electron/electron/main-with-provider.cjs
+++ desktop-electron/electron/main-with-provider.cjs
@@ -1,5 +1,34 @@
+"use strict";
+
 const electron = require("electron");
+const {
+  clearHeadlessExecutionHost,
+  registerHeadlessExecutionHost,
+} = require("./headless-execution-registry.cjs");
 const { installProviderNetwork } = require("./provider-bootstrap.cjs");
+
+const headlessModulePath = require.resolve("./headless-host.cjs");
+const headlessModule = require(headlessModulePath);
+
+class RegisteredHeadlessHost extends headlessModule.HeadlessHost {
+  constructor(options) {
+    super(options);
+    registerHeadlessExecutionHost(this);
+  }
+
+  async shutdown(...args) {
+    try {
+      return await super.shutdown(...args);
+    } finally {
+      clearHeadlessExecutionHost(this);
+    }
+  }
+}
+
+require.cache[headlessModulePath].exports = Object.freeze({
+  ...headlessModule,
+  HeadlessHost: RegisteredHeadlessHost,
+});
 
 installProviderNetwork(electron);
 require("./main.cjs");
