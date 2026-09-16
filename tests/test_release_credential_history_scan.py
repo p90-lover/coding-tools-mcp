5adb2883d40779282d8831ac711017d6a4a99fd4
p90-lover <146848721+p90-lover@users.noreply.github.com>
2026-09-16 21:22:11 +0800

    test(rc2): require newly verified package fixture blob

--- tests/test_release_credential_history_scan.py
+++ tests/test_release_credential_history_scan.py
@@ -13,6 +13,7 @@ EXPECTED_SYNTHETIC_BLOBS = {
     "c32549ef3ff736ceff8aaae026657c70abff5120",
     "ad88631f3124c58730dbe666dbfd23590e4eec3a",
     "d23defc0909cc32314272135d4e09d4f743bd3ba",
+    "af51b2e406e77316b42c65ef2712e64b4acb7447",
 }
 
 
